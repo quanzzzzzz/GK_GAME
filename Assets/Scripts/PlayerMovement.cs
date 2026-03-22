f6747d4d59a9d770f36b919d291cc3b7d2116586
quanzzzzzz <149055440+quanzzzzzz@users.noreply.github.com>
2026-03-22 16:38:01 +0700

    8
    8

--- Assets/Scripts/PlayerMovement.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class PlayerMovement : MonoBehaviour {
-    void Update() {
-        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        worldPoint.z = 0;
-        transform.position = worldPoint;
-    }
-}
